f71091cb83fcb6e27001d255ce27136388f97d3a
Satyam <68682757+satyam2337@users.noreply.github.com>
2025-05-13 17:30:37 +0530

    Delete DANLC/LAB2_12_05_2025 directory

--- DANLC/LAB2_12_05_2025/Q3.py
+++
@@ -1,8 +0,0 @@
-p_amount=int(input("Enter the Purchase amount: "))
-if p_amount >= 500:
-    print("Shipping is free.")
-
-else:
-    print("Shipping is 50.")
-    total=p_amount+50
-print(f"Total amount is {total}.")
